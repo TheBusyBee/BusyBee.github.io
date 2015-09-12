3acfd80b632a9b075c93ef27682d9c5c0ac58758
Pete Werner <pewerner@comcast.net>
2015-09-12 16:34:13 -0400

    First commit

---
+++ flicks-html/js/rev-custom.js
@@ -0,0 +1,26 @@
+/* 
+ * flicks
+ * Created by codeflicks
+ * V1.0
+ */
+
+
+//full width revolution
+
+jQuery(document).ready(function() {
+
+    revapi = jQuery('.fullwidthbanner').revolution(
+            {
+                delay: 9000,
+                startwidth: 1170,
+                startheight: 650,
+                hideThumbs: 10,
+                fullWidth: "on",
+                touchenabled: "on",
+                navigationStyle: "preview4"
+            });
+
+});	//ready
+
+
+
